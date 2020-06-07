5c4e9b39f051be31d5ed69ba3cf3baac1cdd3d00
Silas Almeida <silasadssn@gmail.com>
2020-06-07 16:11:38 -0300

    database config

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,8 +4,9 @@ import path from 'path';
 const connection = knex({
   client: 'sqlite3',
   connection: {
-    filename: path.resolve('src', 'database', 'database.sqlite'),
+    filename: path.resolve(__dirname, '..', 'database', 'database.sqlite'),
   },
+  useNullAsDefault: true,
 });
 
 export default connection;
